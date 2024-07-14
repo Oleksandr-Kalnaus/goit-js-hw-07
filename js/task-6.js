da97f0d678f47dea6aaf916adc82e6d0dd82fa82
Oleksandr-Kalnaus <oleksandrkalnaus@gmail.com>
2024-07-14 20:04:13 +0100

    Edit task 6
    Edit some styles

--- js/task-6.js
+++ js/task-6.js
@@ -1,3 +1,35 @@
+const numberOfBoxes = document.querySelector('#controls input');
+const addButton = document.querySelector(`button[data-create]`);
+const destroyButton = document.querySelector('button[data-destroy]');
+const boxList = document.querySelector("#boxes");
+
+function createBoxes(amount) {
+  destroyBoxes();
+
+  for (let i = 0; i < amount; i++) {
+    const elem = document.createElement(`div`);
+    elem.class = `box`;
+    elem.style.width = `${30 + (10 * i)}px`;
+    elem.style.height = `${30 + (10 * i)}px`;
+    elem.style.backgroundColor = getRandomHexColor();
+    boxList.append(elem);
+  }
+}
+
+addButton.addEventListener('click', function() {
+  const amount = parseInt(numberOfBoxes.value, 10);
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    numberOfBoxes.value = '';
+  }
+});
+
+destroyButton.addEventListener('click', destroyBoxes);
+
+function destroyBoxes() {
+  boxList.innerHTML = '';
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
